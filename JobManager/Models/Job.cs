c9cdf0b90856eb1726ac594d9a9d3174aeb88977
ParwinderDeepKaur <A00237487@mycambrian.ca>
2022-02-13 04:36:22 -0500

    Added model class for Jobs to hold the id, name and other related data. Created an interface that contains the tasks like add single job, list of jobs, delete, update, etc. Create new tasks which are asynchroonous in this interface. Create a new class that will job list in the actual device in text file in json format. Implement the IJobDataStore interface in this class, made sure all the methods are of async type because the api will returning the data asynchronounsly.

---
+++ JobManager/Models/Job.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobManager.Models
+{
+    public class Job
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+    }
+}
