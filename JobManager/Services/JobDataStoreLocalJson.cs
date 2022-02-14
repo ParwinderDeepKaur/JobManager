27f36896c10fb61ee892d6f3847a71d3055a69fa
ParwinderDeepKaur <A00237487@mycambrian.ca>
2022-02-14 11:06:30 -0500

    2:14:00

--- JobManager/Services/JobDataStoreLocalJson.cs
+++ JobManager/Services/JobDataStoreLocalJson.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using JobManager.Models;
+using Newtonsoft.Json;
 
 namespace JobManager.Services
 {
     class JobDataStoreLocalJson : IJobDataStore<Job>
     {
+        public static string FilePath
+        {
+            get
+            {
+                var basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                return Path.Combine(basePath, "Jobs.json");
+            }
+        }
+
         public async Task AddJob(Job job)
         {
             throw new NotImplementedException();
@@ -25,19 +36,46 @@ namespace JobManager.Services
 
         public async Task<IEnumerable<Job>> GetJobs()
         {
-            var jobs = new List<Job>(){
-                new Job{Id = 1, Name = "job A local json file", Description = "this is a job A." },
-                new Job{Id = 1, Name = "job B local json file", Description = "this is a job B." },
-                new Job{Id = 1, Name = "job C local json file", Description = "this is a job C." },
-                new Job{Id = 1, Name = "job D local json file", Description = "this is a job D." }
-            };
-            //throw new NotImplementedException();
+            var jobs = ReadFile();
             return jobs;
         }
 
         public async Task UpdateJob(Job job)
         {
             throw new NotImplementedException();
+        }
+
+        private void WriteFile(List<Job> jobs)
+        {
+            var jsonString = JsonConvert.SerializeObject(jobs);
+            File.WriteAllText(FilePath, jsonString);
+        }
+
+        private List<Job> ReadFile()
+        {
+            if (File.Exists(FilePath))
+            {
+                var jsonString = File.ReadAllText(FilePath);
+                var jobs = JsonConvert.DeserializeObject<List<Job>>(jsonString);
+                return jobs;
+            }
+            else
+            {
+                var jobs = GetDefaultJobs();
+                WriteFile(jobs);
+                return jobs;
+            }
+        }
+
+        private List<Job> GetDefaultJobs()
+        {
+            var jobs = new List<Job>(){
+                new Job{Id = 1, Name = "job A local json file", Description = "this is a job A." },
+                new Job{Id = 1, Name = "job B local json file", Description = "this is a job B." },
+                new Job{Id = 1, Name = "job C local json file", Description = "this is a job C." },
+                new Job{Id = 1, Name = "job D local json file", Description = "this is a job D." }
+            };
+            return jobs;
         }
     }
 }
--- JobManager/ViewModels/JobListViewModel.cs
+++ JobManager/ViewModels/JobListViewModel.cs
@@ -23,12 +23,15 @@ namespace JobManager.ViewModels
 
         public JobListViewModel() {
             Title = "Jobs";
+            Jobs = new ObservableRangeCollection<Job>();
+            LoadJobs();
             RefreshCommand = new AsyncCommand(Refresh);
             SelectCommand = new AsyncCommand<Job>(Selected);
         }
 
-        public async Task Selected(Job job) { 
-            // Not implemented
+        public async Task Selected(Job job) {
+            //string route = $"{nameof(Views.JobDetailPage)}?JobId ={ job.Id}";
+            //await AppShell.Current.GoToAsync(route);
         }
 
         public async Task Refresh() {
