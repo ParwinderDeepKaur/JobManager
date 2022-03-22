5d1a5a74695ff63dc71b794b4b22ea2b3fd32215
ParwinderDeepKaur <A00237487@mycambrian.ca>
2022-03-22 10:51:53 -0400

    added delete button to job list item, added click event to button, call deleteAsync method on button click

--- JobManager/Views/JobListPage.xaml.cs
+++ JobManager/Views/JobListPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using JobManager.Models;
 using JobManager.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -14,16 +15,32 @@ namespace JobManager.Views
     public partial class JobListPage : ContentPage
     {
         JobListViewModel _viewModel;
+        JobManagerBase _JobManagerBase;
         public JobListPage()
         {
             InitializeComponent();
             BindingContext = _viewModel = new JobListViewModel();
+            _JobManagerBase = new JobManagerBase();
         }
-
+        
         protected override void OnAppearing()
         {
             base.OnAppearing();
             _viewModel.Refresh();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void deleteRecord(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+
+            var data = btn.BindingContext as Job;
+            var getId = data.Id;
+            await _JobManagerBase.JobDataStore.DeleteJob(data.Id);
+             _viewModel.Refresh();
         }
     }
 }
