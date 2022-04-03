730a2d60aa3d1928cda1685558ecc242f05b7fdc
ParwinderDeepKaur <A00237487@mycambrian.ca>
2022-04-03 08:22:06 -0400

    created interface and model class for battery device service functionality

---
+++ JobManager.Android/Services/DeviceBatteryService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace JobManager.Droid.Services
+{
+    public class DeviceBatteryService : ContentPage
+    {
+        public DeviceBatteryService()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" }
+                }
+            };
+        }
+    }
+}
